dc4bb8cf4cd8ab7a4bc8233c299eea3b31633cb4
DatDayLay <94358210+DatDayLay@users.noreply.github.com>
2025-07-24 17:01:11 +0100

    modal update

--- risevest-frontend/components/Modal.tsx
+++ risevest-frontend/components/Modal.tsx
@@ -8,9 +8,9 @@ export default function Modal({ isOpen, children }: ModalProps) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-scroll lg:overflow-hidden bg-black/50">
-      <div className="bg-white rounded-xl w-[85%]  lg:w-full max-w-lg py-8 relative">
-        {children}
-      </div>
+      <div className="bg-white rounded-xl w-[95%] max-w-lg lg:w-full py-4 sm:py-6 lg:py-8 relative mx-auto">
+  {children}
+</div>
     </div>
   );
 };
